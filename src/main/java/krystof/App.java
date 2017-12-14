1f551239f877e7ed6cac79510625274ef1e4b544
polansky <polansky@cngroup.dk>
2017-12-14 13:48:51 +0100

    3

--- src/main/java/krystof/App.java
+++ src/main/java/krystof/App.java
@@ -1,14 +1,11 @@
-package krystof.start;
+package krystof;
 
-import krystof.Scan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = Scan.class)
 public class App {
 
     public static void main(String[] args){
--- src/test/java/krystof/Controller/NoteControllerTest.java
+++ src/test/java/krystof/Controller/NoteControllerTest.java
@@ -1,36 +1,58 @@
 package krystof.Controller;
 
+import krystof.Data.Repository;
+import krystof.business.Note;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(NoteController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class NoteControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private Repository repository;
+
+
+    @Before
+    public void setUp() throws Exception {
+when(repository.findOne(anyLong())).thenReturn(new Note("testMock", "testMock"));
+
+    }
+
     @Test
     public void showNote() throws Exception {
-
 
         mockMvc.perform(get("/note/123"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("label")))
-                .andExpect(content().string(containsString("note")));
+                .andExpect(content().string(containsString("note")))
+                .andExpect(content().string(containsString("testMock")));
 
-                //mock reposirotry expect ze se yavola metoda
-        //argthat jakykoliv id
+                
     }
+
+
 
 }
