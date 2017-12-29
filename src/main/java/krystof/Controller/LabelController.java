9d72f9d389db543f1b6e8f51150e8ba83caa0f4f
polansky <kry@pol.com>
2017-12-29 23:17:37 +0100

    frontedn1

--- src/main/java/krystof/Controller/LabelController.java
+++ src/main/java/krystof/Controller/LabelController.java
@@ -3,9 +3,9 @@ package krystof.Controller;
 import krystof.business.Label;
 import krystof.business.NoteHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,11 +26,16 @@ public class LabelController {
         return "savelabel";
     }
 
-    @RequestMapping(value = "/savelabel", method = RequestMethod.POST)
-    public String submitLabel(@ModelAttribute(name = "labelentity") Label labelentity) {
-
-        //todo save it, ale jak to predat view jako novy objekt? lepsi
-        //by bzlo psmeroa tna label a id a dat to tomu id jako parametr
+    @RequestMapping(value = "/savelabel", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String submitLabel(Label label) {
+        System.out.println(label);
+//        Label savedLabel = handler.save(label);
+//        System.out.println("saved" + savedLabel);
+//        System.out.println(model);
+//        model.addAttribute("labelentity", savedLabel);
+//        System.out.println(model);
+//        //todo save it, ale jak to predat view jako novy objekt? lepsi
+//        //by bzlo psmeroa tna label a id a dat to tomu id jako parametr
         return "result";
     }
 
